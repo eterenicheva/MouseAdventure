182e3f2bc751e1c624baecdef3e946190650406f
Ekaterina Terenicheva <Eventcpa@gmail.com>
2020-11-26 22:32:47 +0600

    add coin, enemies

---
+++ Assets/Scripts/CollectCoin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    [SerializeField] private float _waitTime;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<Player>(out Player player))
+        {
+            player.CountCoin(1);
+            gameObject.SetActive(false);
+            Invoke("RespawnCoin", _waitTime);
+            
+        }
+    }
+
+    private void RespawnCoin()
+    {
+        GameObject newCoin = Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        newCoin.SetActive(true);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int _coins;
+
+    public void CountCoin(int count)
+    {
+        _coins += count;
+        Debug.Log(_coins);
+    }
+}
